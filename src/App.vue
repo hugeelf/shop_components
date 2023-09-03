714676a34d1674db33272c43ed9872643c92e5f6
Hugeelf <hugeelf@yandex.ru>
2023-09-03 13:22:25 +0300

    add price modificator homework

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <ProductDetail></ProductDetail>
+  </div>
+</template>
+
+<script>
+import ProductDetail from './components/ProductDetail.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    ProductDetail
+}
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ProductDetail.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <h3>{{ product.name }}</h3>
+        <button v-for="currency in money" :key="currency.id" @click="setCurrency(currency)"> {{ currency }}</button>
+        <br>
+        <p v-if="selectedCurrency === 'RUB'">
+            {{ formattedPriceRub }}
+        </p>
+        <p v-else>
+            {{ formattedPriceUsd }}
+        </p>
+        <p v-if="product.available">
+            Available
+        </p>
+        <p v-else>
+            Our of stock
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductDetail',
+
+    data() {
+        return {
+            product: {
+                name: 'Product name here',
+                price: 99.99,
+                available: true,
+            },
+            money:['RUB', 'USD'],   
+            selectedCurrency: 'USD', 
+            rubCourse: 96,
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        setCurrency(currency){
+            this.selectedCurrency = currency;
+        }
+    },
+    computed: {
+        formattedPriceUsd() {
+            return `$ ${this.product.price}`;
+        },
+        formattedPriceRub(){
+            return `${parseFloat(this.product.price * this.rubCourse).toFixed(2)} RUB`
+        }
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
